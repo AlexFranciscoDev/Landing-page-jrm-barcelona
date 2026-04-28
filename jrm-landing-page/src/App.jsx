e3cf2226ac35389d3ec2407783ed0b902867b665
AlexFranciscoDev <alexbcn98gmail.com>
2026-04-29 00:21:01 +0200

    design: reset styles and css variables

--- jrm-landing-page/src/App.jsx
+++ jrm-landing-page/src/App.jsx
@@ -10,13 +10,13 @@ function App() {
   return (
     <>
       <section id="center">
-        <div className="hero">
+        <div className="hero" >
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1 className="bg-red-700">Get started</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
