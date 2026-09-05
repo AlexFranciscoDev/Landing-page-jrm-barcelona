7f11fb68a4d1149ed089e611d88d3a5e05f38527
AlexFranciscoDev <alexbcn98gmail.com>
2026-09-06 01:17:46 +0200

    fix: footer styles (desktop)

--- jrm-landing-page/src/components/Footer.jsx
+++ jrm-landing-page/src/components/Footer.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import './Footer.css';
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer className='footer-container'>
       <div className="container">
-        <div>
+        <div className='footer-content'>
           <div className="footer-logo">
-            <a href="#home" className="navbar__logo">
+            <a href="#home">
               <img
-                src="/jrm-logo.png"
+                src="/jrm-logo-white.png"
                 alt="Jesus Reigns Ministries Barcelona"
               />
             </a>
-            <p className="footer-about-us">Lorem ipsum</p>
+            <p className="footer-about-us">We are a multicultural Christian church in the heart of Barcelona, growing together in faith, hope, and the love of Jesus.</p>
           </div>
           <div className="footer-links">
             <h3>Links</h3>
@@ -45,7 +45,7 @@ export const Footer = () => {
             </ul>
           </div>
           <div className="footer-contact">
-            <h3>Links</h3>
+            <h3>Contact</h3>
             <ul>
               <li>
                 <a className="footer-link" href="#home">
@@ -65,14 +65,16 @@ export const Footer = () => {
             </ul>
           </div>
         </div>
+
         <hr />
+
         <div className="legal-container">
             <div>
                 © 2025 Jesus Reigns Ministries Barcelona. Todos los derechos reservados.
             </div>
-            <div>
-                <span>Privacy policy</span>
-                <span>Legal notice</span>
+            <div className='legal-docs'>
+                <span><a href='#'>Privacy policy</a></span>
+                <span><a href='#'>Legal notice</a></span>
             </div>
         </div>
       </div>
